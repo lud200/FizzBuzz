78fe8e7ee08c4ce038709768b5d328a0c826f5aa
dludaya <dludaya.cs@gmail.com>
2014-02-25 02:31:04 -0500

    FizzBuzz

---
+++ FizzBuzz/src/FizzBuzz/FizzBuzz.java
@@ -0,0 +1,23 @@
+package FizzBuzz;
+
+public class FizzBuzz {
+	public static void main(String[] args){
+		int a, b, c;
+		for(int i=1; i<101; i++){
+			if(i%3==0 && i%5==0){
+				System.out.println("FizzBuzz");
+			}
+			else if(i%3==0 && i%5!=0){
+				System.out.println("Fizz");
+			}
+			else if(i%5==0 && i%3!=0){
+				System.out.println("Buzz");
+			}
+			
+			else{
+				System.out.println(i);
+			}
+		}
+	}
+
+}
